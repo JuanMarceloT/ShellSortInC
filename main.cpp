798abaa8b8e584d8f2150a9c3656d67fb43904b5
Juan Marcelo Trabaina <jmarcelot2013@gmail.com>
2024-03-30 19:51:15 -0300

    Adding the 'gaps' functions and updating the 'shellsort' function

--- main.cpp
+++ main.cpp
@@ -1,98 +1,135 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <ctime>
+    #include <iostream>
+    #include <fstream>
+    #include <sstream>
+    #include <vector>
+    #include <string>
+    #include <ctime>
+    #include <algorithm>
+    #include <chrono>
+    #include <random> 
 
-void printArr(const std::vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++) {
-        std::cout << arr[i] << " ";
+    void printArr(const std::vector<int>& arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
 
-void shellsortAdd(std::vector<int>& x, int h, int initial, int size) {
-    for (int i = initial; i < size; i += h) {
-        for (int j = i - h; j >= 0; j -= h) {
-            if (x[j + h] < x[j]) {
-                int temp = x[j + h];
-                x[j + h] = x[j];
-                x[j] = temp;
+    void ShellSorts(std::vector<int>& arr, std::vector<int> gaps) {
+        int n = arr.size();
+        for (auto gap : gaps) {
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
             }
         }
     }
-}
 
-int GetMaxNumberLessThan(std::vector<int>& arr, int x) {
-    int i = 0;
-    while (true) {
-        if (arr[i] > x) {
-            return i - 1;
+    //Shell sequence
+    std::vector<int> ShellGaps(int n) {
+        std::vector<int> gaps;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            gaps.push_back(gap);
         }
-        i++;
-    }
-}
-
-bool VerifySort(std::vector<int>& arr) {
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i] < arr[i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void ShellSort(std::vector<int>& x) {
-    std::vector<int> shell = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824};
-
-    int size = x.size();
-
-    int index = GetMaxNumberLessThan(shell, size);
-    int i = 0;
-
-    // Start measuring time
-    clock_t begin = clock();
-
-    do {
-        shellsortAdd(x, shell[index], i, size);
-        i++;
-        index--;
-    } while (index >= 0);
-
-    // Stop measuring time and calculate the elapsed time
-    clock_t end = clock();
-    double elapsed = double(end - begin) / CLOCKS_PER_SEC;
-
-    if (VerifySort(x))
-        std::cout << "To sort " << size << " elements, take " << elapsed << " seconds" << std::endl;
-    else
-        std::cout << "Sort ERROR" << std::endl;
-}
-
-int main() {
-    std::ifstream file("entrada2.txt");
-    std::string line;
-
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        std::vector<std::string> tokens;
-        std::string token;
-
-        // Read tokens and store them in a vector
-        while (iss >> token) {
-            tokens.push_back(token);
-        }
-        int size = std::stoi(tokens[0]);
-
-        // Process tokens and convert to integers
-        std::vector<int> numbers;
-        for (size_t i = 1; i < tokens.size(); ++i) {
-            numbers.push_back(std::stoi(tokens[i]));
-        }
-
-        ShellSort(numbers);
+        return gaps;
     }
 
-    return 0;
-}
+    //Ciura gap sequence
+    std::vector<int> CiuraGaps(int n) {
+        std::vector<int> gaps = {701, 301, 132, 57, 23, 10, 4, 1};
+        while (gaps.back() >= n) {
+            gaps.pop_back();
+        }
+        return gaps;
+    }
+
+    // Knuth gap sequence
+    std::vector<int> KnuthGaps(int n) {
+        std::vector<int> gaps;
+        int gap = 1;
+        while (gap < n) {
+            gaps.push_back(gap);
+            gap = 3 * gap + 1;
+        }
+        
+        if (!gaps.empty()) {
+            std::reverse(gaps.begin(), gaps.end());
+        }
+        
+        return gaps;
+    }
+
+
+
+    int main() {
+        std::ifstream file("entrada2.txt");
+        std::string line;
+
+
+        //reading file
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            std::vector<std::string> tokens;
+            std::string token;
+
+            while (iss >> token) {
+                tokens.push_back(token);
+            }
+            int size = std::stoi(tokens[0]);
+
+            std::vector<int> numbers;
+            for (size_t i = 1; i < tokens.size(); ++i) {
+                numbers.push_back(std::stoi(tokens[i]));
+            }
+        const int n = 1000;
+        
+
+
+        //Shell gap sequence
+
+        std::vector<int> shellTest(numbers);
+
+        auto startShell = std::chrono::steady_clock::now(); 
+        ShellSorts(shellTest, ShellGaps(shellTest.size()));
+        auto endShell = std::chrono::steady_clock::now();
+
+        //Ciura gap sequence
+
+        std::vector<int> CiuraTest(numbers);
+
+        auto startCiura = std::chrono::steady_clock::now();
+        ShellSorts(CiuraTest, CiuraGaps(CiuraTest.size()));
+        auto endCiura = std::chrono::steady_clock::now();
+
+        //Knuth gap sequence
+
+        std::vector<int> KnuthTest(numbers);
+        auto startKnuth = std::chrono::steady_clock::now();
+        ShellSorts(KnuthTest, KnuthGaps(KnuthTest.size()));
+        auto endKnuth = std::chrono::steady_clock::now();
+
+
+
+        
+
+
+        // Print the execution times
+        std::cout<<"Sorting "<<numbers.size()<<std::endl;
+        std::cout << "Shell Sort time: "
+                << std::chrono::duration_cast<std::chrono::nanoseconds>(endShell - startShell).count() /1000000.0
+                << " milliseconds" << std::endl;
+        std::cout << "Ciura Sort time: "
+                << std::chrono::duration_cast<std::chrono::nanoseconds>(endCiura - startCiura).count() /1000000.0
+                << " milliseconds" << std::endl;
+        std::cout << "Knuth Sort time: "
+                << std::chrono::duration_cast<std::chrono::nanoseconds>(endKnuth - startKnuth).count() /1000000.0
+                << " milliseconds" << std::endl;
+
+        }
+
+        return 0;
+    }
